6f577233af71880ee1b69e2793a37cba626cc056
pawelgerard <pawel.gerard.lato@gmail.com>
2021-01-06 15:42:00 +0100

    added some comments

--- modelNN.py
+++ modelNN.py
@@ -2,7 +2,6 @@ import snake
 import tensorflow as tf
 import numpy as np
 import random
-import math
 
 
 class SnakeNN:
@@ -21,18 +20,7 @@ class SnakeNN:
 
         return model
 
-    def get_best_option_for_move(self, probability_model, status):
-        predictions = []
-        for i in range(-1, 2):
-            predictions.append(
-                probability_model.predict(np.array([status[0], status[1], status[2], status[3], status[4],
-                                                    status[5], status[6], status[7], status[8], status[9], status[10],
-                                                    i]).reshape(-1, 12)))
-        possibilities = np.array(predictions).reshape(-1, 9)
-        best_effect = [possibilities[0][2], possibilities[0][5], possibilities[0][8]]
-        return random.choices([-1, 0, 1], best_effect, k=1)
-
-
+    # reward 0 if snake is dead, 1 if not 2 if snake is closer to apple after move
     def rate_move(self, is_snake_dead, distance_before, distance_after):
         if is_snake_dead:
             return 0
@@ -42,7 +30,23 @@ class SnakeNN:
             else:
                 return 1
 
-    def train(self, repeats, predict_model, is_shown, model, probability_model, snake_size):
+    # Simulates all 3 options: turn right, turn left and go forward
+    def get_best_option_for_move(self, probability_model, status):
+        predictions = []
+        # -1 right, 0 forward, 1 left
+        for i in range(-1, 2):
+            predictions.append(
+                probability_model.predict(np.array([status[0], status[1], status[2], status[3], status[4],
+                                                    status[5], status[6], status[7], status[8], status[9], status[10],
+                                                    i]).reshape(-1, 12)))
+        possibilities = np.array(predictions).reshape(-1, 9)
+        # check which of three options (turn right, turn left or go forward) have the highest chance for max reward
+        best_effect = [possibilities[0][2], possibilities[0][5], possibilities[0][8]]
+        # each option is chosen by assigned possibilities in 'best effect' array
+        # mechanism to avoid snake's loops
+        return random.choices([-1, 0, 1], best_effect, k=1)
+
+    def train(self, repeats, model, probability_model, prediction_by_nn=False, visualisation=False, snake_size=3):
         movement_history = []
         movement_effect = []
         for i in range(repeats):
@@ -51,20 +55,19 @@ class SnakeNN:
             while not game.is_snake_dead():
                 status = game.check_status()
                 distance_before = game.get_distance_to_apple()
-                if predict_model == "random":
-                    move = random.choice([-1, 0, 1])
-                    number_of_epochs = 3
+                if not prediction_by_nn:
+                    move = random.choice([-1, 0, 1])  # random choice in case NN predictions are off
+                    number_of_epochs = 5  # to better train model we use 5 epochs
                 else:
                     move = self.get_best_option_for_move(probability_model, status)[0]
-                    number_of_epochs = 10
-                game.game_step(is_shown, move)
+                    number_of_epochs = 1
+                game.game_step(visualisation, move)
                 distance_after = game.get_distance_to_apple()
                 movement_history.insert(0, np.concatenate((status, [move])))
                 movement_effect.insert(0, self.rate_move(game.is_snake_dead(), distance_before, distance_after))
             print(game.score)
         model.fit(np.array(movement_history).reshape(-1, 12), np.array(movement_effect).reshape(-1, 1),
-                  epochs=number_of_epochs,
-                  shuffle=True)
+                  epochs=number_of_epochs, shuffle=True)
         movement_history.clear()
         movement_effect.clear()
 
@@ -73,10 +76,11 @@ class SnakeNN:
         model = self.create_model()
         probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
 
-        self.train(50, "random", False, model, probability_model, 6)
-        for i in range(1):
-            self.train(4, "NN", True, model, probability_model, 3)
-
+        # model is trained with random data at first (1000 games)
+        # starting snake size is 6 for better data
+        self.train(1000, model, probability_model, prediction_by_nn=False, visualisation=False, snake_size=6)
+        # 5 games with visualisation by AI  to check snake's behaviour and score
+        self.train(5, model, probability_model, prediction_by_nn=True, visualisation=True, snake_size=3)
 
 
 SnakeNN().main()
--- snake.py
+++ snake.py
@@ -6,128 +6,144 @@ import math
 class Snake:
 
     def __init__(self, board_size, snake_size):
-        self.unit_size = 16
-        self.board_size = board_size
-        self.screen_size = (board_size + 2) * 16
-        self.border_coordinates = self.define_border_coordiantes()
-        self.snake_coordinates = self.set_snake_size(snake_size)
+        self._unit_size = 16
+        self._board_size = board_size
+        self._screen_size = (board_size + 2) * 16
+        self._border_coordinates = self._define_border_coordiantes()
+        self._snake_coordinates = self._set_snake_size(snake_size)
         self.score = snake_size * 10
-        self.apple_coordinates = self.set_apple_coordinates()
-        self.dict_val = {"north": {0: [0, -self.unit_size], -1: [-self.unit_size, 0], 1: [self.unit_size, 0]},
-                         "east": {0: [self.unit_size, 0], -1: [0, -self.unit_size], 1: [0, +self.unit_size]},
-                         "west": {0: [-self.unit_size, 0], -1: [0, +self.unit_size], 1: [0, -self.unit_size]},
-                         "south": {0: [0, +self.unit_size], -1: [self.unit_size, 0], 1: [-self.unit_size, 0]}}
-        self.dict_dir = {"north": {0: "north", -1: "west", 1: "east"},
+        self._apple_coordinates = self._set_apple_coordinates()
+        # dict describes snake's head coordinates due to direction change
+        self._dict_val = {"north": {0: [0, -self._unit_size], -1: [-self._unit_size, 0], 1: [self._unit_size, 0]},
+                         "east": {0: [self._unit_size, 0], -1: [0, -self._unit_size], 1: [0, +self._unit_size]},
+                         "west": {0: [-self._unit_size, 0], -1: [0, +self._unit_size], 1: [0, -self._unit_size]},
+                         "south": {0: [0, +self._unit_size], -1: [self._unit_size, 0], 1: [-self._unit_size, 0]}}
+        # next direction after particular move
+        # only 3 options: 0 same dir, 1 left, -1 right
+        self._dict_dir = {"north": {0: "north", -1: "west", 1: "east"},
                          "east": {0: "east", -1: "north", 1: "south"},
                          "west": {0: "west", -1: "south", 1: "north"},
                          "south": {0: "south", -1: "east", 1: "west"}}
-        self.last_direction = "north"
+        self._last_direction = "north"
 
-    def set_snake_size(self, snake_size):
+    def _set_snake_size(self, snake_size):
         snake = []
         for i in range(snake_size):
-            snake.append((self.screen_size / 2, self.screen_size / 2 + self.unit_size * i))
+            snake.append((self._screen_size / 2, self._screen_size / 2 + self._unit_size * i))
         return snake
 
-    def define_border_coordiantes(self):
+    def _define_border_coordiantes(self):
         border_coordinates = []
-        for i in range(self.board_size + 2):
-            for j in range(self.board_size + 2):
-                if (i == 0 or j == 0) or (i == self.board_size + 1 or j == self.board_size + 1):
-                    border_coordinates.append((i * self.unit_size, j * self.unit_size))
+        for i in range(self._board_size + 2):
+            for j in range(self._board_size + 2):
+                # only first or last values for border
+                if (i == 0 or j == 0) or (i == self._board_size + 1 or j == self._board_size + 1):
+                    border_coordinates.append((i * self._unit_size, j * self._unit_size))
         return border_coordinates
 
-    def update_snake(self, move, is_apple_eaten):
-        self.snake_coordinates.insert(0, (self.snake_coordinates[0][0] + self.dict_val[self.last_direction][move][0],
-                                          self.snake_coordinates[0][1] + self.dict_val[self.last_direction][move][1]))
+    def _update_snake(self, move, is_apple_eaten):
+        # dict tells what is next coordinate after particular move
+        self._snake_coordinates.insert(0, (self._snake_coordinates[0][0] + self._dict_val[self._last_direction][move][0],
+                                           self._snake_coordinates[0][1] + self._dict_val[self._last_direction][move][1]))
+        # move = additional part of snake body
+        # if apple is not eaten last part of snake body should be deleted
         if not is_apple_eaten:
-            self.snake_coordinates.pop(-1)
-        return self.dict_dir[self.last_direction][move]
+            self._snake_coordinates.pop(-1)
+        return self._dict_dir[self._last_direction][move]
 
     def is_snake_dead(self):
-        snake_body = self.snake_coordinates[1:]
-        head = self.snake_coordinates[0]
-        if (head in self.border_coordinates) or (head in snake_body):
+        snake_body = self._snake_coordinates[1:]
+        head = self._snake_coordinates[0]
+        if (head in self._border_coordinates) or (head in snake_body):
             return True
         return False
 
+    # for NN 11 bool variables
+    # first 3 describes is obstacles on right, forward or left
+    # next 4: what snake direction is: north, east, south or west
+    # last 4: is apple on north, east, south or west
     def check_status(self):
-        result = []
-        head_x = self.snake_coordinates[0][0]
-        head_y = self.snake_coordinates[0][1]
-        apple_x = self.apple_coordinates[0]
-        apple_y = self.apple_coordinates[1]
-        snake_body = self.snake_coordinates[1:-1]
+        obstacle = []
+        head_x = self._snake_coordinates[0][0]
+        head_y = self._snake_coordinates[0][1]
+        apple_x = self._apple_coordinates[0]
+        apple_y = self._apple_coordinates[1]
+        snake_body = self._snake_coordinates[1:-1]
+        # -1 right, 0 forward, 1 left
         for i in range(-1, 2):
-            if ((head_x + self.dict_val[self.last_direction][i][0],
-                 head_y + self.dict_val[self.last_direction][i][1]) in self.border_coordinates) or (
-                    (head_x + self.dict_val[self.last_direction][i][0],
-                     head_y + self.dict_val[self.last_direction][i][1]) in snake_body):
-                result.append(True)
+            if ((head_x + self._dict_val[self._last_direction][i][0],
+                 head_y + self._dict_val[self._last_direction][i][1]) in self._border_coordinates) or (
+                    (head_x + self._dict_val[self._last_direction][i][0],
+                     head_y + self._dict_val[self._last_direction][i][1]) in snake_body):
+                obstacle.append(True)
             else:
-                result.append(False)
-        direction_north = True if self.last_direction == "north" else False
-        direction_east = True if self.last_direction == "east" else False
-        direction_south = True if self.last_direction == "south" else False
-        direction_west = True if self.last_direction == "west" else False
+                obstacle.append(False)
+        direction_north = True if self._last_direction == "north" else False
+        direction_east = True if self._last_direction == "east" else False
+        direction_south = True if self._last_direction == "south" else False
+        direction_west = True if self._last_direction == "west" else False
         apple_north = True if apple_y < head_y else False
         apple_east = True if apple_x > head_x else False
         apple_south = True if apple_y > head_y else False
         apple_west = True if apple_x < head_x else False
-        return [result[0], result[1], result[2], direction_north, direction_east, direction_south, direction_west,
+        return [obstacle[0], obstacle[1], obstacle[2], direction_north, direction_east, direction_south, direction_west,
                 apple_north, apple_east, apple_south, apple_west]
 
-    def set_apple_coordinates(self):
+    # random coodrinates for apple
+    def _set_apple_coordinates(self):
         search_for_coordinates = True
         while search_for_coordinates:
-            apple_x = random.randint(1, self.board_size) * self.unit_size
-            apple_y = random.randint(1, self.board_size) * self.unit_size
-            if (apple_x, apple_y) not in self.snake_coordinates:
+            apple_x = random.randint(1, self._board_size) * self._unit_size
+            apple_y = random.randint(1, self._board_size) * self._unit_size
+            if (apple_x, apple_y) not in self._snake_coordinates:
                 search_for_coordinates = False
         return (apple_x, apple_y)
 
-    def is_apple_eaten(self):
-        head = self.snake_coordinates[0]
-        if head == self.apple_coordinates:
+    def _is_apple_eaten(self):
+        head = self._snake_coordinates[0]
+        if head == self._apple_coordinates:
             return True
         else:
             return False
 
+    # distance cannot exceed 1 (feature for NN)
     def get_distance_to_apple(self):
-        head_x = self.snake_coordinates[0][0]
-        head_y = self.snake_coordinates[0][1]
-        apple_x = self.apple_coordinates[0]
-        apple_y = self.apple_coordinates[1]
+        head_x = self._snake_coordinates[0][0]
+        head_y = self._snake_coordinates[0][1]
+        apple_x = self._apple_coordinates[0]
+        apple_y = self._apple_coordinates[1]
         distance = math.sqrt(math.pow(apple_x - head_x, 2) + math.pow(apple_y - head_y, 2))
-        max_distance = math.sqrt(math.pow(self.board_size * 16, 2) + math.pow(self.board_size * 16, 2))
+        max_distance = math.sqrt(math.pow(self._board_size * 16, 2) + math.pow(self._board_size * 16, 2))
         return distance / max_distance
 
-    def draw_board(self, screen):
+    def _draw_board(self, screen):
         screen.fill((255, 255, 255))
-        for x, y in self.border_coordinates:
-            pg.draw.rect(screen, (0, 100, 0), [x, y, self.unit_size, self.unit_size])
+        for x, y in self._border_coordinates:
+            pg.draw.rect(screen, (0, 100, 0), [x, y, self._unit_size, self._unit_size])
 
-    def draw_snake(self, screen):
-        screen.blit(pg.image.load("head.png"), (self.snake_coordinates[0][0], self.snake_coordinates[0][1]))
-        for coordinate in self.snake_coordinates[1:]:
+    def _draw_snake(self, screen):
+        screen.blit(pg.image.load("head.png"), (self._snake_coordinates[0][0], self._snake_coordinates[0][1]))
+        for coordinate in self._snake_coordinates[1:]:
             screen.blit(pg.image.load("body.png"), (coordinate[0], coordinate[1]))
 
-    def draw_apple(self, screen):
-        screen.blit(pg.image.load("apple.png"), self.apple_coordinates)
+    def _draw_apple(self, screen):
+        screen.blit(pg.image.load("apple.png"), self._apple_coordinates)
 
-    def game_step(self, visualization, move):
-        if visualization:
+    # optional visualisation is feature for NN
+    # it allows algorithm to learn faster
+    def game_step(self, visualisation, move):
+        if visualisation:
             pg.init()
-            screen = pg.display.set_mode((self.screen_size, self.screen_size))
+            screen = pg.display.set_mode((self._screen_size, self._screen_size))
             fps_clock = pg.time.Clock()
-            self.draw_board(screen)
-            self.draw_apple(screen)
-            self.draw_snake(screen)
+            self._draw_board(screen)
+            self._draw_apple(screen)
+            self._draw_snake(screen)
             pg.display.update()
             fps_clock.tick(2)
-        if self.is_apple_eaten():
-            self.last_direction = self.update_snake(move, True)
-            self.apple_coordinates = self.set_apple_coordinates()
+        if self._is_apple_eaten():
+            self._last_direction = self._update_snake(move, True)
+            self._apple_coordinates = self._set_apple_coordinates()
             self.score += 10
         else:
-            self.last_direction = self.update_snake(move, False)
+            self._last_direction = self._update_snake(move, False)
